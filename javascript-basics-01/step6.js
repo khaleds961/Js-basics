74f3c5c487ceefbdcaad93b177b592d3fe30d0f2
khaled al houssein <khaledalhoussein3@gmail.com>
2021-03-09 13:47:22 +0200

    Step 6

---
+++ javascript-basics-01/step6.js
@@ -0,0 +1,6 @@
+function myFunction(){
+    var firstNumber= document.getElementById("first_number").value;
+    var secondNumber= document.getElementById("second_number").value;
+    var product = firstNumber%secondNumber;
+    alert(product);
+}
