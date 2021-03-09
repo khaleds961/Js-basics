6b9e01af265f9b70e39f70204f067ba0ca7d2de9
khaled al houssein <khaledalhoussein3@gmail.com>
2021-03-09 11:59:15 +0200

    Step 4

---
+++ javascript-basics-01/step4.js
@@ -0,0 +1,5 @@
+var name =prompt("what is your Name?"),
+ surname =prompt("what is your Surname?"),
+ city =prompt("what is your City?");
+
+alert("\n Hello"+"\n Name: "+name+"\n Surname:"+surname+"\n City: "+city)
