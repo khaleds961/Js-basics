5946fd3a00820137077b6d63ce952dca994af1bc
khaled al houssein <khaledalhoussein3@gmail.com>
2021-03-10 10:16:23 +0200

    First step

---
+++ javascript-basics-02/step 1/step1.js
@@ -0,0 +1,8 @@
+
+function bigImg() {
+    document.getElementById("image1").src = "images/image1_2.jpg";
+}
+function samepic() {
+    document.getElementById("image1").src = "images/image1.jpg";
+
+}
