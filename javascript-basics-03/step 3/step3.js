f7cab9afa64f2f46ed28896b8189889823d4b401
khaled al houssein <khaledalhoussein3@gmail.com>
2021-03-11 15:46:38 +0200

    Step 3

---
+++ javascript-basics-03/step 3/step3.js
@@ -0,0 +1,12 @@
+var p=document.getElementById("text");
+function myFuction(x){
+    if (x=='green'){
+p.style.color="green";
+    }
+    if (x=='red'){
+        p.style.color="red";
+            }
+            if (x=='blue'){
+                p.style.color="blue";
+                    }
+}
