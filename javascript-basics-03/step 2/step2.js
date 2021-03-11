b95962dd940d8bd8a54e74fe6cdec38d10d4c55a
khaled al houssein <khaledalhoussein3@gmail.com>
2021-03-11 15:30:23 +0200

    Step 3

---
+++ javascript-basics-03/step 2/step2.js
@@ -0,0 +1,16 @@
+var c = document.getElementById("texte");
+function myFunction(x) {
+    if (x == 'show') {
+        c.style.display = "block";
+    }
+    if (x == 'hide') {
+        c.style.display = "none";
+    }
+}
+
+
+
+
+
+
+
